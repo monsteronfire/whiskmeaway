e1887442d433dccbcc4eb16a0a26b774468109a1
monsteronfire <monsteronfire@gmail.com>
2014-10-23 11:26:19 +0800

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,85 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      options: {
+        includePaths: ['bower_components/scss']
+      },
+      dist: {
+        options: {
+          outputStyle: 'compressed'
+        },
+        files: {
+          'css/app.css': 'scss/app.scss'
+        }
+      }
+    },
+
+    copy: {
+      scripts: {
+        expand: true,
+        cwd: 'bower_components/',
+        src: '**/*.js',
+        dest: 'js'
+      },
+
+      maps: {
+        expand: true,
+        cwd: 'bower_components/',
+        src: '**/*.map',
+        dest: 'js'
+      }
+    },
+
+    // Task configuration.
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: [
+          'js/init.js',
+          'js/plugins.js'
+        ],
+
+        dest: 'js/app.js'
+      }
+
+    },
+
+    uglify: {
+      dist: {
+        files: {
+          'js/modernizr/modernizr.min.js': ['js/modernizr/modernizr.js']
+        }
+      }
+    },
+
+    watch: {
+      grunt: { files: ['Gruntfile.js'] },
+
+      sass: {
+        files: 'scss/**/*.scss',
+        tasks: ['sass']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-modernizr');
+
+  // Default task.
+  grunt.registerTask('build', ['sass']);
+  grunt.registerTask('default', ['copy', 'uglify', 'concat', 'watch']);
+
+};
